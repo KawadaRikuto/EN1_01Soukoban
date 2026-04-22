f08365bb32809009a346eb676028518f8dfbc6f8
LC1B_10_Kawada_Rikuto <k024g0092@g.neec.ac.jp>
2026-04-22 14:36:00 +0900

    プロジェクト ファイルを追加します。

---
+++ Source/EN1_01Soukoban/EN1_01Soukoban.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EN1_01Soukoban.h"
+
+EN1_01Soukoban::EN1_01Soukoban()
+{
+}
+
+EN1_01Soukoban::~EN1_01Soukoban()
+{
+}
---
+++ Source/EN1_01Soukoban/EN1_01Soukoban.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class EN1_01SOUKOBAN_API EN1_01Soukoban
+{
+public:
+	EN1_01Soukoban();
+	~EN1_01Soukoban();
+};
